2631ad893ca50f15f75369eadd9f49d3c68e9397
ashtneoi <ashtneoi@gmail.com>
2020-01-05 00:18:44 -0800

    Get a simple program working!!!!!
    ```none
    riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt \
        -bios ~/ashtos/ashtos.bin
    ```

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_builtins]
+#![no_main]
+#![feature(global_asm)]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        // do nothing
+    }
+}
+
+global_asm!(r#"
+    .global go
+
+    .section .vector
+
+    go:
+        li x6, 0x5555
+        li x7, 0x100000
+        sh x6, 0(x7)
+"#);
+
+#[no_mangle]
+pub extern "C" fn hi() { }
