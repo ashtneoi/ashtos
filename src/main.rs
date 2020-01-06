5e9fd4424e5f037a7efb020828b6d3a7efae416c
ashtneoi <ashtneoi@gmail.com>
2020-01-05 18:25:35 -0800

    Do polled UART echoer

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,31 @@ global_asm!(r#"
     .section .vector
 
     go:
-        li x6, 0x13333
-        li x7, 0x100000
-        sw x6, 0(x7)
+        j uart_echo_loop
+        li t0, 0x93333
+        li t1, 0x100000
+        sw t0, 0(t1)
+
+    uart_write_byte:
+        li t0, 0x10000000
+        sb a0, 0(t0)
+        ret
+
+    uart_read_byte_noblock:
+        li t0, 0x10000000
+        lb t1, 5(t0)
+        andi t2, t1, 0x01
+        bne t2, zero, _urbn1
+        li a1, 1
+        ret
+    _urbn1:
+        lb a0, 0(t0)
+        li a1, 0
+        ret
+
+    uart_echo_loop:
+        call uart_read_byte_noblock
+        bne a1, zero, uart_echo_loop
+        call uart_write_byte
+        j uart_echo_loop
 "#);
